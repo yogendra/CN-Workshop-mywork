dfb6ced481b83cce28c90f6b68b895e7666a2376
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-01-02 10:58:42 +0800

    Add Feign client to application class

--- cloud-native-spring-ui/src/main/java/io/pivotal/cloudnativespringui/CloudNativeSpringUiApplication.java
+++ cloud-native-spring-ui/src/main/java/io/pivotal/cloudnativespringui/CloudNativeSpringUiApplication.java
@@ -1,14 +1,28 @@
 package io.pivotal.cloudnativespringui;
 
+import io.pivotal.cloudnativespring.domain.City;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableFeignClients
+@EnableDiscoveryClient
 public class CloudNativeSpringUiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudNativeSpringUiApplication.class, args);
 	}
 
+	@FeignClient("cloud-native-spring")
+	public interface CityClient {
+		@GetMapping(value = "/cities", consumes = "application/hal+json")
+		Resources<City> getCities();
+	}
 }
 
