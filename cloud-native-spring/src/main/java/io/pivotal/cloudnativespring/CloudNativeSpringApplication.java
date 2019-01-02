d097a968aa22d3977d195b90bd47f1f8eca37286
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-01-02 09:40:38 +0800

    Update Application class to use @Value

--- cloud-native-spring/src/main/java/io/pivotal/cloudnativespring/CloudNativeSpringApplication.java
+++ cloud-native-spring/src/main/java/io/pivotal/cloudnativespring/CloudNativeSpringApplication.java
@@ -1,20 +1,26 @@
 package io.pivotal.cloudnativespring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @RestController
 public class CloudNativeSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudNativeSpringApplication.class, args);
 	}
+
+	@Value("${greeting:Hola}")
+	private String greeting;
+
 	@RequestMapping("/")
 	public String hello() {
-		return "Hello World!";
+		return greeting + " World!";
 	}
 }
 
