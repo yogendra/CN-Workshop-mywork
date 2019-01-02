5be00ea3a9e883d6dd5f8b1bee7bf0c854f304c3
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-01-02 11:07:40 +0800

    Add Application UI

--- cloud-native-spring-ui/src/main/java/io/pivotal/cloudnativespringui/AppUI.java
+++ cloud-native-spring-ui/src/main/java/io/pivotal/cloudnativespringui/AppUI.java
@@ -1,5 +1,30 @@
 package io.pivotal.cloudnativespringui;
 
-public class AppUI {
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import io.pivotal.cloudnativespring.domain.City;
+import io.pivotal.cloudnativespringui.CloudNativeSpringUiApplication.CityClient;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@Theme(Lumo.class)
+@Route("")
+public class AppUI extends VerticalLayout {
+
+  private final CityClient cityClient;
+
+  private final Grid<City> grid;
+
+  @Autowired
+  public AppUI(CityClient cityClient) {
+    this.cityClient = cityClient;
+
+    grid = new Grid<>(City.class);
+    add(grid);
+    grid.setWidth("100");
+    grid.setHeight("100");
+    grid.setItems(cityClient.getCities().getContent());
+  }
 }
