a57c15226806d47e19df59761e4088da8fee9f44
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-01-02 10:19:23 +0800

    Add @EnableDiscoveryClient annotation

--- cloud-native-spring/src/main/java/io/pivotal/cloudnativespring/CloudNativeSpringApplication.java
+++ cloud-native-spring/src/main/java/io/pivotal/cloudnativespring/CloudNativeSpringApplication.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableDiscoveryClient
 @RestController
 public class CloudNativeSpringApplication {
 
